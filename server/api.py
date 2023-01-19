1796ee6cdf0a6398a069bbc94daee6da1785dfe9
isavega <igvega2@gmail.com>
2023-01-19 00:56:46 -0300

    feat: crete fastapi app

--- server/api.py
+++ server/api.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
-from .routes import router as NoteRouter
+from .routes import router as ApplicantRouter
 
 app = FastAPI()
 
 @app.get("/", tags=["Root"])
 async def read_root() -> dict:
     return {
-        "message": "Welcome to my notes application, use the /docs route to proceed"
+        "message": "¡Bienvenido a Tenpo Lab! La documentación está en la ruta /docs"
     }
 
-app.include_router(NoteRouter, prefix="/note")
+app.include_router(ApplicantRouter, prefix="/trainee", tags=["Trainee"])
